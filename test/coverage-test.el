79b63f9de726446d15e4cbf6471b319d1e052733
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2016-02-02 22:32:33 +1300

    [WIP] Add test for coverage-get-json-from-file & extract variables

--- test/coverage-test.el
+++ test/coverage-test.el
@@ -4,13 +4,26 @@
 (require 'ert)
 (require 'coverage)
 
-(ert-deftest coverage-test-get-results-for-file-1 ()
-  (should (equal (coverage-get-results-for-file "/foo/bar.rb" "./example.json")
-                 '(1 0 nil))))
+(defvar bar-results [1 0 nil])
+(defvar quux-results  [nil 1 nil nil 0 0 16])
 
-(ert-deftest coverage-test-get-results-for-file-2 ()
+(defvar expected-json `((RSpec
+                         (timestamp . 1453405229)
+                         (coverage
+                          '(/foo/bar\.rb . ,bar-results)
+                          '(/baz/qux/quux\.rb . ,quux-results)))))
+
+(ert-deftest test-coverage-get-json-from-file ()
+  (should (equal (coverage-get-json-from-file "./example.json")
+                 expected-json)))
+
+(ert-deftest test-coverage-get-results-for-file-bar ()
+  (should (equal (coverage-get-results-for-file "/foo/bar.rb" "./example.json")
+                 bar-results)))
+
+(ert-deftest test-coverage-get-results-for-file-quux ()
   (should (equal (coverage-get-results-for-file "/baz/qux/quux.rb" "./example.json")
-                 '(1 5 0 nil 1 5 nil nil 1 nil nil 0 0 16 nil))))
+                 quux-results)))
 
 (provide 'coverage-test)
 
